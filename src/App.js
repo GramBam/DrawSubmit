f91295bd8041d4b8106abd560847799adea6aa82
Graham <graham.moss@hotmail.com>
2022-05-11 17:29:22 -0400

    more features and styling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ function App() {
   const canvasRef = useRef(null)
   const contextRef = useRef(null)
   const [drawing, setDrawing] = useState(false)
-  const [color, setColor] = useState('black')
 
   useEffect(() => {
     const canvas = canvasRef.current
-    canvas.width = window.innerWidth * 2
-    canvas.height = window.innerHeight * 2
-    canvas.style.width = `${window.innerWidth}px`
-    canvas.style.height = `${window.innerHeight}px`
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    canvas.style.width = `${window.innerWidth / 2}px`
+    canvas.style.height = `${window.innerHeight / 2}px`
 
     const context = canvas.getContext('2d')
     context.scale(2, 2)
@@ -45,24 +44,52 @@ function App() {
   }
 
   const colorChange = (e) => {
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
+    const context = canvasRef.current.getContext('2d')
     context.strokeStyle = e.target.id
     contextRef.current = context
   }
 
+  const brushChange = (e) => {
+    const context = canvasRef.current.getContext('2d')
+    context.lineWidth = parseInt(e.target.id)
+    contextRef.current = context
+  }
+
+  const saveImage = () => {
+    const canvas = canvasRef.current
+    let image = canvas.toDataURL('image/jpeg');
+    console.log(image);
+  }
+
+  const clear = () => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
   return (
     <>
-      <button id='black' onClick={colorChange}>BLACK</button>
-      <button id='blue' onClick={colorChange}>BLUE</button>
-      <button id='red' onClick={colorChange}>RED</button>
-      <canvas
-        onMouseDown={drawStart}
-        onMouseUp={drawEnd}
-        onMouseMove={draw}
-        ref={canvasRef}
-      />
+      <container>
+        <nav>
+          <button id='black' onClick={colorChange}>BLACK</button>
+          <button id='blue' onClick={colorChange}>BLUE</button>
+          <button id='red' onClick={colorChange}>RED</button>
+          <button id='white' onClick={colorChange}>ERASER</button>
+          <button id='20' onClick={brushChange}>Big Brush</button>
+          <button id='3' onClick={brushChange}>Small Brush</button>
+          <button onClick={clear}>Clear</button>
+          <button onClick={saveImage}>Submit</button>
+        </nav>
+
+        <canvas
+          onMouseDown={drawStart}
+          onMouseUp={drawEnd}
+          onMouseMove={draw}
+          ref={canvasRef}
+        />
+      </container>
     </>
+
   );
 }
 
