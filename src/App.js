d352ceb6013e4b32ef9361b2e4ad9389bd9f6da3
Graham <graham.moss@hotmail.com>
2022-05-11 16:14:21 -0400

    purge boilerplate, start simple draw functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState } from "react";
 
 function App() {
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [drawing, setDrawing] = useState(false)
+  const [color, setColor] = useState('black')
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    canvas.width = window.innerWidth * 2
+    canvas.height = window.innerHeight * 2
+    canvas.style.width = `${window.innerWidth}px`
+    canvas.style.height = `${window.innerHeight}px`
+
+    const context = canvas.getContext('2d')
+    context.scale(2, 2)
+    context.lineCap = 'round'
+    context.strokeStyle = 'black'
+    context.lineWidth = 5
+    contextRef.current = context
+  }, [])
+
+
+  const drawStart = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setDrawing(true)
+  }
+
+  const drawEnd = () => {
+    contextRef.current.closePath()
+    setDrawing(false)
+  }
+
+  const draw = ({ nativeEvent }) => {
+    if (!drawing) {
+      return
+    }
+    const { offsetX, offsetY } = nativeEvent
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
+
+  const colorChange = (e) => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    context.strokeStyle = e.target.id
+    contextRef.current = context
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button id='black' onClick={colorChange}>BLACK</button>
+      <button id='blue' onClick={colorChange}>BLUE</button>
+      <button id='red' onClick={colorChange}>RED</button>
+      <canvas
+        onMouseDown={drawStart}
+        onMouseUp={drawEnd}
+        onMouseMove={draw}
+        ref={canvasRef}
+      />
+    </>
   );
 }
 
