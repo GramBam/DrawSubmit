79fa3f6583b7fd84282a17230df919673b848bea
Graham <graham.moss@hotmail.com>
2022-05-13 13:38:19 -0400

    prepare for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -16,5 +17,14 @@ app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => { res.status(200).json({ message: ':)' }) })
 
 app.use('/api/entries', require('./routes/entryRoutes'))
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+  app.get('/', (req, res) => { res.status(200).json({ message: ':)' }) })
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
