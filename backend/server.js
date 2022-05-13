ad66824dc802ff0e697cc1295f75edb827ecf07a
Graham <graham.moss@hotmail.com>
2022-05-13 14:31:21 -0400

    more server fixes for heroku

--- backend/server.js
+++ backend/server.js
@@ -13,18 +13,17 @@ const app = express()
 //Body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-app.get('/', (req, res) => { res.status(200).json({ message: ':)' }) })
-
 app.use('/api/entries', require('./routes/entryRoutes'))
 
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
-  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+  app.get('*', (_, res) => { res.sendFile(path.join(__dirname, '../frontend/build/index.html')) })
 } else {
-  app.get('/', (req, res) => { res.status(200).json({ message: ':)' }) })
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Something is wrong :)' })
+  })
 }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
