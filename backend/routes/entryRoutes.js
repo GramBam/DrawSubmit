e39bf2308fcc520318661624872ea11a70d6871c
Graham <graham.moss@hotmail.com>
2022-05-14 15:11:22 -0400

    remove bodyparser

--- backend/routes/entryRoutes.js
+++ backend/routes/entryRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getEntries, createEntry, getLatest } = require('../controllers/entryController')
 
-router.route('/').get(getEntries).post(express.bodyParser({ limit: '50mb' }), createEntry)
+router.route('/').get(getEntries).post(createEntry)
 router.route('/latest').get(getLatest)
 
 module.exports = router
